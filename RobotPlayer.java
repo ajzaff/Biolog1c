ff7b2b06f16f9abe0dd4142a11b930c288db10a7
Alan Zaffetti <azaffett@umass.edu>
2015-01-06 04:10:08 -0500

    a little more.

--- RobotPlayer.java
+++ RobotPlayer.java
@@ -21,8 +21,6 @@ This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
-
-
 */
 
 package biolog1c;
@@ -53,9 +51,21 @@ public class RobotPlayer {
   static final Random rand = new Random();
 
   /**
+   * The maximum health an HQ can sense to cause a
+   * cutoff in the target-selection routine.
+   */
+  static final double HQ_AUTO_ENGAGE_CUTOFF = 24;
+
+  /**
    * The tower's squared-range constant.
    */
   static final int TOWER_RANGE = 24;
+
+  /**
+   * The maximum health a tower can sense to cause a
+   * cutoff in the target-selection routine.
+   */
+  static final double TOWER_AUTO_ENGAGE_CUTOFF = 8;
 
   /**
    * A static copy of the array of directional constants.
@@ -74,6 +84,8 @@ public class RobotPlayer {
     // Assign the enemy team instance.
     otherTeam = myTeam.opponent();
 
+    int i = 0;
+
     /*
      * The method is encapsulated in a forever-loop.
      * It should run for as long as the match running.
@@ -84,41 +96,126 @@ public class RobotPlayer {
       RobotType t = rc.getType();
 
       if(t == HQ) {
-        // The `HQ' can spawn beavers, and attack other things, if needed.
+        /*
+         * The `HQ' can spawn beavers, and attack things, if needed.
+         */
+
+        // Test if the HQ's weapon is ready.
+        if(rc.isWeaponReady()) {
+          /**
+           * Although we do not want it to come down to this,
+           * the HQ is capable of doing large amounts of damage
+           * to targets.
+           *
+           * This routine seeks out the best such target to hit, if any.
+           */
+
+          RobotInfo[] targets = rc.senseNearbyRobots(rc.getLocation(), t.attackRadiusSquared, otherTeam);
+
+          // If no nearby enemies, yield and continue.
+          if(targets.length > 0) {
+            try {
+              RobotInfo t_info = targets[0]; // The "best target" -- let's initialize this to `target_0'.
+
+              // Loop through all enemy units
+              for (RobotInfo r : targets) {
+
+                // If the target is a missile, cause a cutoff.
+                if (r.type == MISSILE) {
+                  t_info = r;
+                  break;
+                }
+
+                // If the new target is less healthy -- update it.
+                if (r.health < t_info.health) {
+
+                  // If the health is "low enough" -- cutoff.
+                  if (r.health <= HQ_AUTO_ENGAGE_CUTOFF) {
+                    t_info = r;
+                    break;
+                  }
+
+                  t_info = r; // Update the target info variable.
+                }
+              }
+
+              rc.attackLocation(t_info.location); // Attack it!
+            } catch (GameActionException e) {
+              e.printStackTrace();
+            }
+          }
+        }
 
         Direction d = directions[rand.nextInt(8)];
-        if(rc.isCoreReady() && rc.canSpawn(d, BEAVER)) {
-          try {
-            rc.spawn(d, BEAVER);
-          }
-          catch(GameActionException e) {
-            e.printStackTrace();
+        if(rc.isCoreReady()) {
+          if(i < 20) {
+            if(rc.canSpawn(d, BEAVER)) {
+              try {
+                rc.spawn(d, BEAVER);
+                i++;
+              }
+              catch(GameActionException e) {
+                e.printStackTrace();
+              }
+            }
           }
         }
       }
       else if(t == TOWER) {
-        // The `tower' is a powerful unit which attacks things.
-        // It has an effective attack range of 24 units^2.
+        /*
+         * The `tower' is a powerful unit which attacks things.
+         * It has an effective attack range of 24 units^2 (see TOWER_RANGE).
+         */
 
+        // Test if the tower's weapon system is online.
         if(rc.isWeaponReady()) {
-          RobotInfo[] robots = rc.senseNearbyRobots(rc.getLocation(), TOWER_RANGE, otherTeam);
 
+          // Get an array of nearby robots within this towers attacking range.
+          RobotInfo[] targets = rc.senseNearbyRobots(rc.getLocation(), TOWER_RANGE, otherTeam);
+
+          // If no nearby enemies, yield and continue.
+          if(targets.length == 0) {
+            rc.yield();
+            continue;
+          }
+
+          /*
+           * The goal is to find a suitable enemy robot to attack.
+           * The strategy is generally, to find the robot
+           * with the minimum remaining health and attack it.
+           *
+           * To save compute time, if a target with less than a given
+           * cutoff is found, it is selected, and target search stops.
+           *
+           * Missile targets prioritized over all others.
+           */
           try {
-            double minHealth = Integer.MAX_VALUE;
-            double thresholdHealth = 5;
-            RobotInfo r_info = null;
-            for(RobotInfo r : robots) {
-              if(r.health < minHealth) {
-                if(r.health < thresholdHealth) {
-                  r_info = r;
+
+            RobotInfo t_info = targets[0]; // The "best target" -- let's initialize this to `target_0'.
+
+            // Loop through all enemy units
+            for(RobotInfo r : targets) {
+
+              // If the target is a missile, cause a cutoff.
+              if(r.type == MISSILE) {
+                t_info = r;
+                break;
+              }
+
+              // If the new target is less healthy -- update it.
+              if(r.health < t_info.health) {
+
+                // If the health is "low enough" -- cutoff.
+                if(r.health <= TOWER_AUTO_ENGAGE_CUTOFF) {
+                  t_info = r;
                   break;
                 }
-                r_info = r;
-                minHealth = r.health;
+
+                t_info = r; // Update the target info variable.
               }
             }
-            if(r_info != null)
-              rc.attackLocation(robots[rand.nextInt(robots.length)].location);
+
+            rc.attackLocation(t_info.location); // Attack it!
           }
           catch(GameActionException e) {
             e.printStackTrace();
@@ -132,7 +229,11 @@ public class RobotPlayer {
         // Soldiers walk around and attack things.
       }
       else if(t == BEAVER) {
-        // Beavers are units which BUILD structures (like BARRACKS).
+        /*
+         * Beavers are the only units capable of building structures.
+         * They also have a limited ability to mine, and attack if needed.
+         */
+
         if(rc.isCoreReady()) {
           if(rand.nextBoolean()) {
             Direction d = directions[rand.nextInt(8)];
@@ -143,25 +244,14 @@ public class RobotPlayer {
                 e.printStackTrace();
               }
             }
-            else {
-              try {
-                rc.mine();
-              }
-              catch(GameActionException e) {
-                e.printStackTrace();
-              }
-            }
           }
-          else if(rand.nextBoolean()) {
+          else if(rc.senseOre(rc.getLocation()) >= 1) {
             try {
               rc.mine();
             }
             catch(GameActionException e) {
               e.printStackTrace();
             }
-          }
-          else {
-
           }
         }
       }
